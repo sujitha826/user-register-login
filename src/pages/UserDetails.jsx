9ae2a77dea215d41de3bc60650467e61c3bb6232
Sujitha K <sujitha.k@marketsimplified.com>
2022-09-21 11:13:48 +0530

    details-with-redux-store

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -3,10 +3,11 @@ import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from "react-router-dom";
 import LogoutIcon from '@mui/icons-material/Logout';
+import { connect } from 'react-redux';
 
-const UserDetails = () => {
+const UserDetails = (props) => {
 
-    const [logindata, setLoginData] = useState([]);
+    const [loginData, setLoginData] = useState([]);
     const history = useNavigate();
 
     const [show, setShow] = useState(false);
@@ -17,21 +18,34 @@ const UserDetails = () => {
     const handleShow = () => setShow(true);
 
     const Birthday = () => {
-        const getUser = localStorage.getItem("login_user");
-        
-        if (getUser && getUser.length) {
-            const user = JSON.parse(getUser);
-            console.log(user);
-            setLoginData(user);
-            console.log(JSON.stringify(logindata[0]));
+        // const getUser = localStorage.getItem("login_user");
+        // console.log(getUser);
+        // if (getUser && getUser.length) {
+        //     const user = JSON.parse(getUser);
+        //     console.log(user);
+        //     setLoginData(user);
+        //     console.log(JSON.stringify(logindata[0]));
 
-            const userbirth = logindata.map((el, k) => {
+        //     const userbirth = logindata.map((el, k) => {
+        //         return el.dob === todaysDate;
+        //     });
+        //     console.log(userbirth);
+        //     if (userbirth[0]) {
+        //         setTimeout(() => {
+        //             console.log(`Today is the Bday of ${logindata[0].name}!!`);
+        //             handleShow();
+        //         }, 3000);
+        //     }
+        // }
+        const user = props.loginUser ? props.loginUser : [];
+        setLoginData(user);
+        if (loginData[0]) {
+            const userbirth = loginData.map((el, k) => {
                 return el.dob === todaysDate;
             });
-            console.log(userbirth);
             if (userbirth[0]) {
                 setTimeout(() => {
-                    console.log(`Today is the Bday of ${logindata[0].name}!!`);
+                    console.log(`Today is the Bday of ${loginData[0].name}!!`);
                     handleShow();
                 }, 3000);
             }
@@ -50,16 +64,16 @@ const UserDetails = () => {
     return (
         <>
             {
-                logindata.length === 0 ? "error" :
+                loginData.length === 0 ? "Oops!!! Login user not found..Please re-login after a while" :
                     <>
                         <h2>User Details</h2>
-                        <h3>Hi!! {logindata[0].name}</h3>
+                        <h3>Hi!! {loginData[0].name}</h3>
                         <Button onClick={userLogout}><LogoutIcon /> LogOut</Button>
                         {
-                            logindata[0].dob === todaysDate ?
+                            loginData[0].dob === todaysDate ?
                                 <Modal show={show} onHide={handleClose}>
                                     <Modal.Header closeButton>
-                                        <Modal.Title>{logindata[0].name} 😄</Modal.Title>
+                                        <Modal.Title>{loginData[0].name} 😄</Modal.Title>
                                     </Modal.Header>
 
                                     <Modal.Body>Wish you many many happy returns of the day!! Have a wonderful and joyful day..🍰</Modal.Body>
@@ -81,4 +95,11 @@ const UserDetails = () => {
     );
 }
 
-export default UserDetails;
+function mapStateToProps(store) {
+    console.log("store", store);
+    return {
+        loginUser: store.loginNow.loginUser
+    }
+}
+
+export default connect(mapStateToProps, null)(UserDetails);
